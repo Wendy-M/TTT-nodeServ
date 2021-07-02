e50e726a0f1efbcb44176c96a0594bdc8a8ab8d3
Wendy-M <wendymontagnon@gmail.com>
2021-07-02 17:05:40 +0200

    maj

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4dd7d03744f0031e65ff686498bdc131.js"
+  "/precache-manifest.9f69027e17b41e84ba4a003dbf9929aa.js"
 );
 
 self.addEventListener('message', (event) => {
